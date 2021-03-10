64676c4e718ab0a809de609066e8ca72e866dc3a
JareBear12418 <jaredgrozz@gmail.com>
2021-03-10 08:15:20 -0600

    LOGOS!!!

--- logos_class.py
+++ logos_class.py
@@ -1,5 +1,28 @@
 class Logos_Names:
+    '''
+    Logos name, and path to that logo.
+
+    Very important that names_class.py and this script have the same name, otherwise it will probaly
+    crash, havent tested that yet, but im confident it will.
+    '''
     def __init__(self):
         self.Paths: dict = {
-            'Python':'logos/python.png'
+            'C':'logos/C.png',
+            'C++':'logos/C++.png',
+            'C#':'logos/C#.png',
+            'CSS':'logos/CSS.png',
+            'GO':'logos/GO.png',
+            'Haskell':'logos/Haskell.png',
+            'html':'logos/html.png',
+            'Java':'logos/Java.png',
+            'JavaScript':'logos/JavaScript.png',
+            'TypeScript':'logos/TypeScript.png',
+            'Kotlin':'logos/Kotlin.png',
+            'Lua':'logos/Lua.png',
+            'php':'logos/php.png',
+            'Python':'logos/Python.png',
+            'R':'logos/R.png',
+            'Ruby':'logos/Ruby.png',
+            'Swift':'logos/Swift.png',
+            'TypeScript':'logos/TypeScript.png',
         }
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import json
 import time
+import os
+import random
+
+HOME: str = os.path.expanduser("~")
 
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
@@ -8,33 +12,29 @@ from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 import numpy as np
 from git import Repo
 
-import cv2
-import numpy as np
-
 from names_class import Language_Names
 from logos_class import Logos_Names
-
+# Get better language names from language names class
+language_class_names_old, language_class_names_new = zip(*Language_Names().Names.items())
+logo_name, logo_path = zip(*Logos_Names().Paths.items())
+programming_languages = Language_Names().Programming_Names
 plt.style.use("dark_background")
 
-PATH_TO_TIMETRAKED_JSON_FILE: str = 'C:\\Users\\grossj\\.vscode\\extensions\\fabriciorojas.localtimetracker-1.0.6\\timeTrakedL.json'
+PATH_TO_TIMETRAKED_JSON_FILE: str = f'{HOME}/.vscode/extensions/fabriciorojas.localtimetracker-1.0.6/timeTrakedL.json'
 # cd /home/jared/Documents/Python-Projects/Activity-Tracker/JareBear12418/; /usr/bin/env /usr/local/bin/python3.8 /home/jared/Documents/Python-Projects/Activity-Tracker/JareBear12418/main.py
 
-def img_to_pie( fn, wedge, xy, zoom=1, ax = None):
-    if ax is None: ax=plt.gca()
-    im = plt.imread(fn, format='png')
-    path = wedge.get_path()
-    patch = PathPatch(path, facecolor='none')
-    ax.add_patch(patch)
-    imagebox = OffsetImage(im, zoom=zoom, clip_path=patch, zorder=-10)
-    ab = AnnotationBbox(imagebox, xy, xycoords='data', pad=0, frameon=False)
-    ax.add_artist(ab)
+
+
+SHOW_ONLY_PROGRAMMING_LANGUAGES: bool = False
+SHOW_ICONS: bool = True
+
 
 def update_file_to_commit():
     with open(PATH_TO_TIMETRAKED_JSON_FILE, 'r') as f: timeTrakedL = json.load(f)
 
     time_in_each_lang = {}
     dict_lang_names = []
-    all_used_languages = []
+    all_extensions = []
     time_in_each_lang_KEYS = []
     time_in_each_lang_VALUES = []
 
@@ -42,20 +42,22 @@ def update_file_to_commit():
     dict_lang_names = [list(i['languageTime'].keys()) for i in timeTrakedL['dates']]
 
     # Add all the language names into one list
-    all_used_languages = [item for lst in dict_lang_names for item in lst]
+    all_extensions = [item for lst in dict_lang_names for item in lst]
 
     # Remove duplicate languages
-    all_used_languages = list(set(all_used_languages))
+    all_extensions = list(set(all_extensions))
 
-    # Get better language names from language names class
-    language_class_names_old, language_class_names_new = zip(*Language_Names().Names.items())
+    # filter out all extensions to leave only programming languages.
+    if SHOW_ONLY_PROGRAMMING_LANGUAGES: all_extensions = [extension for extension in all_extensions for pLangauge in programming_languages if extension == pLangauge]
 
     # Make a dictionary with all the language names in place
-    time_in_each_lang = {langauge:[] for langauge in all_used_languages}
+    time_in_each_lang = {langauge:[] for langauge in all_extensions}
 
     # Add all times in each language into respective language name creating a list of all the times.
     for i, d1 in enumerate(timeTrakedL['dates']):
-        for name in d1['languageTime']: time_in_each_lang[name].append(d1['languageTime'].get(name))
+        for name in d1['languageTime']:
+            try: time_in_each_lang[name].append(d1['languageTime'].get(name))
+            except KeyError: pass #It can't find that extension in the languageTime dicionary
 
     # addying the sum of each list to get one number and not a list of numbers.
     for i in time_in_each_lang: time_in_each_lang[i] = (sum(time_in_each_lang[i]))
@@ -77,18 +79,18 @@ def update_file_to_commit():
         time_in_each_lang_KEYS.append(key + f' - {time_formating}')
         time_in_each_lang_VALUES.append(value)
 
-    def combine_column_names(column_name,
-                             cur_value,
+    def combine_language_names(language_name,
+                             percentage,
                              sums):
-        try: column_name = [language_class_names_new[index] for index, name in enumerate(language_class_names_old) if column_name == name][0] # prettify name with langauge class names
+        try: language_name = [language_class_names_new[index] for index, name in enumerate(language_class_names_old) if language_name == name][0] # prettify name with langauge class names
         except IndexError: pass
-        percentage = round(cur_value/sums*100,2)
-        return "{} {}%".format(column_name,percentage)
+        percentage = round(percentage/sums*100,2)
+        return f'{language_name} {percentage}%'
 
     # Create a graph with matplotlib with all the data we have in the time_in_each_lang dictionary.
     data = time_in_each_lang
     base_d = sum(list(data.values()))
-    final_data = {combine_column_names(k,m,base_d):m/base_d*100 for k,m in data.items()}
+    final_data = {combine_language_names(k,m,base_d):m/base_d*100 for k,m in data.items()}
 
     c_list = [np.random.choice(list(mcolors.CSS4_COLORS.values())) for i in range(len(time_in_each_lang_KEYS))]
     _, ax = plt.subplots(figsize=(6, 5),
@@ -100,7 +102,7 @@ def update_file_to_commit():
                        shadow=True,
                        explode=(beat == max(beat)) * 0.1,
                        wedgeprops=dict(
-                           width=0.7,
+                           width=0.67,
                            linewidth=1.5,
                            antialiased=True,
                            edgecolor='k',
@@ -129,25 +131,40 @@ def update_file_to_commit():
                         xy=(x, y),
                         xytext=(1*np.sign(x), 1.4*y),
                         horizontalalignment=horizontalalignment, **kw)
+    if SHOW_ICONS:
+        # Add logo to graph
+        def img_to_pie( fn, wedge, xy, zoom=1, ax = None):
+            if ax is None: ax=plt.gca()
+            im = plt.imread(fn, format='png')
+            patch = PathPatch(wedge.get_path(), facecolor='none')
+            ax.add_patch(patch)
+            imagebox = OffsetImage(im, zoom=zoom, clip_path=patch, zorder=-10)
+            ab = AnnotationBbox(imagebox, xy, xycoords='data', pad=0, frameon=False)
+            ax.add_artist(ab)
 
-    # Get better language names from language names class
-    logo_name, logo_path = zip(*Logos_Names().Paths.items())
-    for index, wedge in enumerate(wedges):
-        for i, name in enumerate(logo_name):
-            if time_in_each_lang_KEYS[index].__contains__(name):
-                img_to_pie(logo_path[i], wedge, xy=(-0.3,0.3), zoom=0.5,ax=ax)
-                wedge.set_zorder(3)
+        locations: list = [(0.2, 0.3),(0.2, -0.3),(-0.2, 0.3),(-0.2, -0.3)]
+        locations2: list = [(0.55, -0.5)]
+        for index, wedge in enumerate(wedges):
+            # wedge.set_facecolor("none") # makes transparent
+            for i, name in enumerate(logo_name):
+                if time_in_each_lang_KEYS[index].__contains__(name):
+                    img_to_pie(logo_path[i], wedge, xy=random.choice(locations if data[index] > 30 else locations2), zoom=(
+                        data[index]/100*.35 if data[index] > 10 else
+                        data[index]/70
+                        ),
+                            ax=ax)
+                    wedge.set_zorder(3)
 
     plt.xticks([])
     plt.yticks([])
-    plt.savefig('C:\\Users\\grossj\\Desktop\\Code\\VSCode-Coding-Activity-on-Github-Profile\\stats.png',
+    plt.savefig(f'{str(os.path.dirname(os.path.realpath(__file__)))}/stats.png',
                 bbox_inches='tight',
                 dpi=100,
                 transparent=True)
 FILE_TO_COMMIT_NAME: str = 'stats.png'
 
 def commit_repository():
-    repo = Repo('/home/jared/Documents/Python-Projects/Activity-Tracker/JareBear12418/.git')
+    repo = Repo(f'{str(os.path.dirname(os.path.realpath(__file__)))}/.git')
     repo.index.add([FILE_TO_COMMIT_NAME])
     repo.index.commit(f'stats.png Updated!')
     origin = repo.remote('origin')
